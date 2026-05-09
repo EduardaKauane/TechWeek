617426e2623c1b9439502dcd496815efe95b93df
Diogotex0 <diogoteixeira4000@gmail.com>
2026-05-09 00:43:56 -0300

    fix: corrige URL da foto dos palestrantes no dashboard (Supabase Storage)

--- js/dashboard.js
+++ js/dashboard.js
@@ -624,7 +624,7 @@ function renderEventSpeakers() {
     <div class="event-item-card" data-id="${s.id}">
       <div class="event-item-photo">
         ${s.foto
-          ? `<img src="${API}${s.foto}" alt="${s.nome}">`
+          ? `<img src="${s.foto.startsWith('http') ? s.foto : API + s.foto}" alt="${s.nome}">`
           : `<div class="photo-placeholder"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z"/><path d="M19 10v2a7 7 0 0 1-14 0v-2"/></svg></div>`}
       </div>
       <div class="event-item-info">
